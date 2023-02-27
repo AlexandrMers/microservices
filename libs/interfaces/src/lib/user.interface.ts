a61b5123315348eaca96a18d6f10fe895647ab50
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2023-02-27 08:21:05 +0300

    libs: add new interface and enum for UserCourses

--- libs/interfaces/src/lib/user.interface.ts
+++ libs/interfaces/src/lib/user.interface.ts
@@ -3,10 +3,22 @@ export enum UserRole {
   STUDENT,
 }
 
+export enum PurchaseState {
+  STARTED = 'started',
+  WAITING_FOR_PAYMENT = 'waitingForPayment',
+  PURCHASED = 'purchased',
+  CANCELLED = 'cancelled',
+}
+
 export interface IUser {
   _id?: string;
   displayName?: string;
   email: string;
   passwordHash: string;
   role: UserRole;
+}
+
+export interface IUserCourses {
+  courseId: string;
+  purchaseState: PurchaseState;
 }
